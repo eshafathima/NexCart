520a94ce673a600b6c91402623e4b549d3407b87
esha fathima <eshafathima202@gmail.com>
2026-06-14 08:28:13 +0530

    Half of user interface

--- products/urls.py
+++ products/urls.py
@@ -15,15 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from . import settings
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('products.urls')),
-    path('customer/',include('customer.urls'))
-
+    path('', views.index, name='Home'),
+    path('products/', views.list_product, name='list_product'),
+    path('product-details/', views.details_product, name='product_details'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def list_product(request):
+    """returns product list page"""
+    return render(request,'products.html')
+
+def details_product(request):
+    return render(request,'product_detail.html')
