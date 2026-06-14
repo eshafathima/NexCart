7d061d1e32aa5e59808c6da86aa0251766292a75
esha fathima <eshafathima202@gmail.com>
2026-06-14 09:14:45 +0530

    user interface half 2

--- orders/urls.py
+++ orders/urls.py
@@ -15,15 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from . import settings
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('products.urls')),
-    path('customer/',include('customer.urls')),
-    path('cart/',include('orders.urls'))
+    path('cart', views.show_cart, name='cart'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
