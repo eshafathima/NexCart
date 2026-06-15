95567784a5ad20e5444d808a9e480f4b8c8950de
esha fathima <eshafathima202@gmail.com>
2026-06-15 19:56:31 +0530

    user authentication

--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.models import User
+from .models import Customer
+# Create your views here.# Create your views here.
 def show_account(request):
+    if request.POST and 'register' in request.POST:
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        email=request.POST.get('email')
+        address=request.POST.get('address')
+        phone=request.POST.get('phone')
+        #create user account
+        user=User.objects.create(
+             username=username,
+             password=password,
+             email=email
+        )
+        customer_obj = Customer.objects.create(  user=user,
+             phone=phone,address=address)
     return render(request,'account.html')
--- products/views.py
+++ products/views.py
@@ -3,14 +3,18 @@ from .models import Product
 from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    featured_product=Product.objects.order_by('priority')[:2]
+    latest_product=Product.objects.order_by('-priority')[:3]
+    context={'featured' :featured_product,
+             'latest':latest_product }
+    return render(request,'index.html',context)
 
 def list_product(request):
     """returns product list page"""
     page=1
     if request.GET:
         page=request.GET.get('page',1)
-    product_list=Product.objects.all()
+    product_list=Product.objects.order_by('priority')
     product_paginator=Paginator(product_list,2)
     product_list=product_paginator.get_page(page )
     context={'Products':product_list}
